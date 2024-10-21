573fcd3b15412fd02527c20301de5a5793dba9d3
reda410 <elhourrireda@gmail.com>
2024-10-21 12:11:36 +0000

    t

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints out output using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programmer, c'est comme construire un puzzle multilingue");
+    return (0);
+}

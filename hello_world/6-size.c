4dbab3220880c6db9689e013f5abcb502202f880
reda410 <elhourrireda@gmail.com>
2024-10-21 12:54:51 +0000

    r

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the sizes of various data types.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("Taille de char: %zu octets\n", sizeof(char));
-    printf("Taille de int: %zu octets\n", sizeof(int));
-    printf("Taille de float: %zu octets\n", sizeof(float));
-    printf("Taille de double: %zu octets\n", sizeof(double));
-    printf("Taille de long: %zu octets\n", sizeof(long));
-    printf("Taille de long long: %zu octets\n", sizeof(long long));
-
-    return 0;
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
 }

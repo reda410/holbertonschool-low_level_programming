871e02961a695b89f20dbc229e5c958aa19e5f95
reda410 <elhourrireda@gmail.com>
2024-10-21 10:03:16 +0000

    t

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+#include <stdio.h>
+
+int main() {
+    puts("Programmer, c'est comme construire un puzzle multilingue");
+    return 0;
 }

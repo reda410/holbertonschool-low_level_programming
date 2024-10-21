b6f75a21268a97846ff9798eed7db3a5ce310004
reda410 <elhourrireda@gmail.com>
2024-10-21 10:09:55 +0000

    t

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+#include <stdio.h>/**
+ * main - Prints out output using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }

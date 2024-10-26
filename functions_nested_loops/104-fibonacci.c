5897c027e8e49099e0c695c62abdf0cdfe3e1472
reda410 <elhourrireda@gmail.com>
2024-10-26 14:42:14 +0000

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,7 @@ for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half > 9999999999)
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
 half2 %= 10000000000;
@@ -37,7 +37,7 @@ if (count != 98)
 printf (", ");
 fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
-fib2_half = half1;
+fib2_half1 = half1;
 fib2_half2 = half2;
 }
 printf("\n");

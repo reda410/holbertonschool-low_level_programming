927fb691a0e785d5fbc56aabc9514602909a6d6c
reda410 <elhourrireda@gmail.com>
2024-10-26 13:19:05 +0000

    101-natural.c

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -4,13 +4,12 @@
  * main - Prints the sum of all multiples of 3 or 5 up to 1024
  * Return: Always (Success)
  */
-
 int main(void)
 {
-int i, z = 0;
+int i = 0, z = 0;
 while (i < 1024)
 {
-if %i 3 == 0) || (i % 5 == 0))
+if ((i % 3 == 0) || (i % 5 == 0))
 {
 z += i;
 }
